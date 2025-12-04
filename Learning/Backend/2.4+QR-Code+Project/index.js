5d23176d69c3d9afec4eb3f9a9ce585690f1bb85
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2025-12-04 17:20:29 +0200

    Create .png for QR image and save user input

--- Learning/Backend/2.4+QR-Code+Project/index.js
+++ Learning/Backend/2.4+QR-Code+Project/index.js
@@ -11,8 +11,18 @@ inquirer
     },
   ])
   .then((answers) => {
-    const text = answers.text;
-    const qrCode = qr.imageSync(text, { type: "svg" });
-    fs.writeFileSync("my_qr_code.svg", qrCode);
-    console.log("qr code generated");
+    const url = answers.text;
+    const qrCode = qr.image(url);
+    qrCode.pipe(fs.createWriteStream("QR_Code.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file have been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Sorry, the prompt can't be shown in this environment.");
+    } else {
+      console.error("Something went wrong:", error);
+    }
   });
