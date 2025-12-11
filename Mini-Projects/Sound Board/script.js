3c98ddbc884d288e401040f1937371f3fcbe17b7
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2025-12-11 23:04:10 +0200

    Implement soundboard with clickable buttons and audio playback

---
+++ Mini-Projects/Sound Board/script.js
@@ -0,0 +1,21 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const buttons = document.getElementById("buttons");
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopAllSounsds();
+    document.getElementById(sound).play();
+  });
+  buttons.appendChild(btn);
+});
+
+function stopAllSounsds() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
