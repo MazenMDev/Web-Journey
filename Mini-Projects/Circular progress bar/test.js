18433edf18db5b144030deea18e04989fd43d0cc
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2025-12-30 02:38:58 +0200

    feat(Circle-progress-bar): take input from user and animate the progress

--- Mini-Projects/Circular progress bar/test.js
+++ Mini-Projects/Circular progress bar/test.js
@@ -1,10 +1,38 @@
-let number = document.getElementById("number");
-let counter = 0;
-let myInterval = setInterval(() => {
-  if (counter == 67) {
-    clearInterval(myInterval);
-  } else {
-    counter++;
-    number.innerHTML = counter + "%";
+const setNumber = document.getElementById("input");
+const setBtn = document.getElementById("btn");
+const pNumber = document.querySelector(".number");
+const circle = document.querySelector("circle");
+
+let animationInterval = null;
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    setBtn.click();
   }
-}, 30);
+});
+
+setBtn.addEventListener("click", () => {
+  let val = parseInt(setNumber.value, 10);
+  if (val > 0 && val <= 100) {
+    animateProgress(val);
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+function animateProgress(target) {
+  clearInterval(animationInterval);
+  let counter = 0;
+  pNumber.innerHTML = "0%";
+  circle.style.strokeDashoffset = 472;
+
+  animationInterval = setInterval(() => {
+    if (counter >= target) {
+      clearInterval(animationInterval);
+    } else {
+      counter++;
+      pNumber.innerHTML = counter + "%";
+      circle.style.strokeDashoffset = 472 - (472 * counter) / 100;
+    }
+  }, 20);
+}
