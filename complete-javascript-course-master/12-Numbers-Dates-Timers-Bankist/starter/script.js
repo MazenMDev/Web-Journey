fedc7116dcec9f19a7ffe6e4f6be82872bbee029
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2026-01-09 21:21:48 +0200

    learn about number conversion and validation examples

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,43 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// converting and checking numbers
+console.log(23 === 23.0); // true
+
+// base 10 - 0 to 9
+// binary base 2 - 0 1
+
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); // false
+
+// conversion
+console.log(Number('23')); // 23
+console.log(+'23'); // 23
+
+// parsing
+console.log(Number.parseInt('30px', 10)); // 30
+console.log(Number.parseInt('e23', 10)); // NaN
+
+console.log(Number.parseFloat('2.5rem')); // 2.5
+console.log(Number.parseFloat('rem2.5')); // NaN
+
+// checking if value is NaN
+console.log(Number.isNaN(20)); // false
+console.log(Number.isNaN('20')); // false
+console.log(Number.isNaN(+'20X')); // true 
+console.log(Number.isNaN(23 / 0)); // false
+
+// checking if value is a number
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20X')); // false
+console.log(Number.isFinite(23 / 0)); // false
+
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(23 / 0)); // false
+console.log(Number.isInteger('23')); // false
+console.log(Number.isInteger(+'23X')); // false
+console.log(Number.isInteger(23.5)); // false
+
