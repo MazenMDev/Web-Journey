4cc985c7b9932f21806942d90b15b2353eed0ede
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2026-01-11 23:50:08 +0200

    practice more on numbers

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -291,3 +291,42 @@ console.log(Number.isInteger('23')); // false
 console.log(Number.isInteger(+'23X')); // false
 console.log(Number.isInteger(23.5)); // false
 
+
+/////////////////////////////////////////////////// Math and Rounding
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+console.log(8 ** (1 / 3)); // 2
+
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+
+console.log(Math.trunc(Math.random() * 6) + 1); // random number between 1 and 6
+
+// create random number between min and max
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+
+// console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.trunc(23.3)); // 23
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor(23.9)); // 23
+
+console.log(Math.floor(-23.3)); // -24
+console.log(Math.trunc(-23.3)); // -23
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // '3' 
